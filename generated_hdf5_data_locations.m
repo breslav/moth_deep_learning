bfa74cadd215b7594f93bb04a70154b90f40ce09
breslav <breslav@bu.edu>
2016-05-13 16:34:35 -0400

    Updated script to generate list of hdf5 files, and then to ensure that subsequent epochs don't see the data in the same exact order the hdf5 are shuffled.

--- generated_hdf5_data_locations.m
+++ generated_hdf5_data_locations.m
@@ -1,15 +1,51 @@
 %generate hdf5 locations test/train
 
-f = fopen('./moth_example/experiments/training_samples/train_file_location_t_20k.txt','w');
+f = fopen('./moth_example/experiments/training_samples/train_file_location_t_20k_rand_epoch.txt','w');
 
-num_batches = 20;
 
-for i=1:1:num_batches
-   if( i < num_batches)
-        fprintf(f,['/research/bats3/Breslav/deeplearning/moth_example/hdf5_data/t/moth_train_',num2str(i),'.hdf5\n']); 
-   else
-        fprintf(f,['/research/bats3/Breslav/deeplearning/moth_example/hdf5_data/t/moth_train_',num2str(i),'.hdf5']);
-   end
+randomize_epochs = true;
+
+total_iterations = 10000;
+batch_size = 32;
+total_samples = batch_size * total_iterations;
+
+hdf5_size = 1000; %number of samples in a single hdf5 file
+
+if(~ randomize_epochs)
+
+    num_hdf5_batches = 20;
+
+    for i=1:1:num_hdf5_batches
+       if( i < num_hdf5_batches)
+            fprintf(f,['/research/bats3/Breslav/deeplearning/moth_example/hdf5_data/t/moth_train_',num2str(i),'.hdf5\n']); 
+       else
+            fprintf(f,['/research/bats3/Breslav/deeplearning/moth_example/hdf5_data/t/moth_train_',num2str(i),'.hdf5']);
+       end
+    end
+else %want to shuffle hdf5 files across multiple epochs
+    num_hdf5_batches_orig = 20;
+
+    for i=1:1:num_hdf5_batches_orig
+            fprintf(f,['/research/bats3/Breslav/deeplearning/moth_example/hdf5_data/t/moth_train_',num2str(i),'.hdf5\n']); 
+    end
+    
+    num_hdf5_batches_additional = ceil((total_samples - (num_hdf5_batches_orig*hdf5_size))/hdf5_size);
+    
+    random_hdf5_idx = 1:1:num_hdf5_batches_additional; %need this many additional hdf5 files to ensure each epoch sees data in different order.
+    random_hdf5_idx = mod(random_hdf5_idx-1,num_hdf5_batches_orig)+1;
+    
+    random_idx = randperm(length(random_hdf5_idx));
+    random_hdf5_idx = random_hdf5_idx(random_idx);
+    
+    for i=1:1:length(random_hdf5_idx)
+        if(i < length(random_hdf5_idx))
+            fprintf(f,['/research/bats3/Breslav/deeplearning/moth_example/hdf5_data/t/moth_train_',num2str(random_hdf5_idx(i)),'.hdf5\n']);
+        else
+            fprintf(f,['/research/bats3/Breslav/deeplearning/moth_example/hdf5_data/t/moth_train_',num2str(random_hdf5_idx(i)),'.hdf5']);
+        end
+    end
+    
+    
 end
 
 fclose(f);
